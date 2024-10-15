e1b3f029dbf3cb97327bb599250df431e099ef11
Laura Jaime <laura.jv@coditramuntana.com>
2024-10-15 17:29:13 +0200

    Fix decorator

--- app/decorators/decidim/user_decorator.rb
+++ app/decorators/decidim/user_decorator.rb
@@ -1,48 +1,46 @@
 # frozen_string_literal: true
 
-module Decidim
-  class UserDecorator
-    def self.decorate
-      Decidim::User.class_eval do
-        def after_confirmation
-          return unless available_members_picker_authorization?
+module Decidim::UserDecorator
+  def self.decorate
+    Decidim::User.class_eval do
+      def after_confirmation
+        return unless available_members_picker_authorization?
 
-          Decidim::Authorization.create_or_update_from(handler)
-        end
+        Decidim::Authorization.create_or_update_from(handler)
+      end
 
-        def grant_authorization
-          after_confirmation
-        end
+      def grant_authorization
+        after_confirmation
+      end
 
-        def email_changed?
-          revoke_members_picker_authorization!
-        end
+      def email_changed?
+        revoke_members_picker_authorization!
+      end
 
-        private
+      private
 
-        def available_members_picker_authorization?
-          organization.available_authorizations.member?(handler_name)
-        end
+      def available_members_picker_authorization?
+        organization.available_authorizations.member?(handler_name)
+      end
 
-        def handler_params
-          @handler_params ||= { user: self, members_picker: email }
-        end
+      def handler_params
+        @handler_params ||= { user: self, members_picker: email }
+      end
 
-        def handler_name
-          @handler_name ||= Decidim::Verifications::MembersPicker::MembersPickerAuthorizationHandler.handler_name
-        end
+      def handler_name
+        @handler_name ||= Decidim::Verifications::MembersPicker::MembersPickerAuthorizationHandler.handler_name
+      end
 
-        def handler
-          @handler ||= Decidim::AuthorizationHandler.handler_for(handler_name, handler_params)
-        end
+      def handler
+        @handler ||= Decidim::AuthorizationHandler.handler_for(handler_name, handler_params)
+      end
 
-        def granted_members_picker_authorization
-          Decidim::Authorization.find_by(decidim_user_id: id, name: handler_name)
-        end
+      def granted_members_picker_authorization
+        Decidim::Authorization.find_by(decidim_user_id: id, name: handler_name)
+      end
 
-        def revoke_members_picker_authorization!
-          granted_members_picker_authorization.destroy! if granted_members_picker_authorization
-        end
+      def revoke_members_picker_authorization!
+        granted_members_picker_authorization.destroy! if granted_members_picker_authorization
       end
     end
   end
